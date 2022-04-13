8c2040d36867ab2685b412cef76703f1e43ad9ef
Konnor Rogers <konnor5456@gmail.com>
2022-04-13 15:12:43 -0400

    vite config fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   clearScreen: false,
   server: {
     fs: {
-      allow: ["/Users/konnorrogers/projects/oss/tip-tap-element/"]
+      allow: ["/Users/konnorrogers/projects/oss/tip-tap-element/", "/home/konnorrogers/projects/tip-tap-element"]
     }
   },
   plugins: [
